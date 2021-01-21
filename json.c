6fd18622aa676a1d6a459f439eb55440f0b76913
Michael Farber Brodsky <taco@mailo.com>
2021-01-21 16:44:36 +0200

    Better digit checking

--- json.c
+++ json.c
@@ -192,7 +192,7 @@ char *parse_string(char **s) {
 double parse_number(char **s) {
     double d;
     sscanf(*s, "%lf", &d);
-    for (; **s == '0' || **s == '1' || **s == '2' || **s == '3' || **s == '4' || **s == '5' || **s == '6' || **s == '7' || **s == '8' || **s == '9' || **s == '.'; ++*s);
+    for (; ('0' <= **s && **s <= '9') || **s == '.'; ++*s);
     return d;
 }
 
